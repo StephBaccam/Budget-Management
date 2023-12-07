1fc4b54d84d9a2473f1492a0ee8dba2ee4103869
StephBaccam <101124726+StephBaccam@users.noreply.github.com>
2023-12-07 15:50:18 +0100

    Delete src/app/firebase.config.ts
    Config file should not be commited

--- src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyC4LGl1kIXL1wnAkdPKxsWJpRaewEJCorg",
-    authDomain: "budget-management-77c08.firebaseapp.com",
-    projectId: "budget-management-77c08",
-    storageBucket: "budget-management-77c08.appspot.com",
-    messagingSenderId: "548330311262",
-    appId: "1:548330311262:web:5732efa5cbe4910831aecf"
-}
