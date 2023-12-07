cdf9a3b5340e9aa3ff020829db522927c22f060c
StephaneBaccam <stephane.baccam@aleysia.com>
2023-12-07 15:53:37 +0100

    remove sensitive data

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,8 +1,8 @@
 export const config = {
-    apiKey: "AIzaSyC4LGl1kIXL1wnAkdPKxsWJpRaewEJCorg",
-    authDomain: "budget-management-77c08.firebaseapp.com",
-    projectId: "budget-management-77c08",
-    storageBucket: "budget-management-77c08.appspot.com",
-    messagingSenderId: "548330311262",
-    appId: "1:548330311262:web:5732efa5cbe4910831aecf"
+    apiKey: "%apiKey%",
+    authDomain: "%authDomain%",
+    projectId: "%projectId%",
+    storageBucket: "%storageBucket%",
+    messagingSenderId: "%messagingSenderId%",
+    appId: "%appId%"
 }
